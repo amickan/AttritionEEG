389c2fb412bd69a7ea148c44ba2524f8d58887d9
amickan <a.mickan@donders.ru.nl>
2018-03-07 14:26:56 +0100

    Finished new marker script

--- NewMarker.m
+++ NewMarker.m
@@ -1,50 +1,20 @@
 function []=NewMarker(pNumber)
 
-% read the familiarization file from Day 1 
-curdir = strcat('\\cnas.ru.nl\wrkgrp\STD-Back-Up-Exp2-EEG\', int2str(pNumber),'\Day 1');
-cd(curdir);
-Fam1FileName=strcat(int2str(pNumber),'_Familiarization_Day1.txt');
-fid = fopen(Fam1FileName);
-C = textscan(fid, '%d%d%d%s%s%s%d%d%d', 'headerlines',1); % 9 columns and 1 headerline
-Item1 = C{4};
-Known = C{9}; % was a word already known in Italian, 1 == yes, 0 == no
-fclose(fid);
-
-% read the familiarization file from Day 3
-curdir = strcat('\\cnas.ru.nl\wrkgrp\STD-Back-Up-Exp2-EEG\', int2str(pNumber),'\Day 3\', int2str(pNumber),'_Familiarization');
-cd(curdir);
-Fam2FileName=strcat(int2str(pNumber),'_IntFamiliarization.txt');
-fid = fopen(Fam2FileName);
-D = textscan(fid, '%d%d%s%s%s%s%s%d%d%d%d', 'headerlines',1); % 11 columns and 1 headerline
-Item2 = D{3};
-Unknown = D{11}; % was a word unknown in English, 1 == yes, 0 == no 
-fclose(fid);
-
 % read the final test file from Day 3
-curdir = strcat('\\cnas.ru.nl\wrkgrp\STD-Back-Up-Exp2-EEG\', int2str(pNumber),'\Day 3\', int2str(pNumber),'_FinalTest');
+curdir = strcat('\\cnas.ru.nl\wrkgrp\STD-Back-Up-Exp2-EEG\', int2str(pNumber),'\Day3\', int2str(pNumber),'_FinalTest');
 cd(curdir);
-FinalTestFileName=strcat(int2str(pNumber),'_FinalTest.txt');
+FinalTestFileName=strcat(int2str(pNumber),'_FinalTest_new.txt');
 fid = fopen(FinalTestFileName);
 headline21=fgets(fid); % read and safe the header of the file for later
-E = textscan(fid, '%d%d%d%s%s%s%d%d%d%d%d%d%s%d'); % 14 columns and 1 headerline
-Subj = E{1};
-Block = E{2};
-Trial = E{3};
-Item3 = E{4};
-LbNL = E{5};
-label = E{6};
+E = textscan(fid, '%d%d%d%s%s%s%d%d%d%d%s%s%s%d%d'); % 14 columns and 1 headerline
+Item = E{4};
 Cond = E{7};
-VoiceOnset = E{8};
 Marker = E{9};
-Error = E{10};
-PhonCorr = E{11};
-PhonIncorr = E{12};
-Produced = E{13};
-TypeError = E{14};
+ReadIn = E{15}; % 1 for include data and 0 for do not include data
 fclose(fid);
 
 % read the old marker file
-curdir = strcat('\\cnas.ru.nl\wrkgrp\STD-Back-Up-Exp2-EEG\', int2str(pNumber),'\Day 3\EEG');
+curdir = strcat('\\cnas.ru.nl\wrkgrp\STD-Back-Up-Exp2-EEG\', int2str(pNumber),'\Day3\EEG');
 cd(curdir);
 dataFileName=strcat(int2str(pNumber),'.vmrk');
 fid = fopen(dataFileName);
@@ -59,6 +29,8 @@ headline8=fgets(fid);
 headline9=fgets(fid);
 headline10=fgets(fid);
 headline11=fgets(fid);
+headline12=fgets(fid);
+headline13=fgets(fid);
 F = textscan(fid, '%s%s%d%d%d','Delimiter',',');
 Stimulus=F{1};
 OldMarker=F{2};
@@ -67,65 +39,30 @@ Length=F{4};
 Channel=F{5};
 fclose(fid);
 
-% sort all three vectors to have the same order (based on the Items order)
-[~,Item1sort]=sort(Item1);
-[~,Item2sort]=sort(Item2);
-Item31 = Item3(1:70);
-[Item3sorted,Item31sort]=sort(Item31);
-Item312 = Item3(71:140);
-[Item32sorted,Item312sort]=sort(Item312);
-Item3sort = [Item31sort;(Item31sort+70)];
-
-Knowncopy=Known(Item1sort);
-Unknowncopy=Unknown(Item2sort);
-Errorcopy=Error(Item3sort);
-
-% compute the new FinalTest error column 
-NewError = Errorcopy;
-for k=1:70
-    if Knowncopy(k)=='1' || Unknowncopy(k)=='1'
-        NewError(k) = '1';
-        NewError(k+70)= '1';
-    end
-end
-
-% convert the new error vector back into the order of the original error
-% vector
-[~,L] = ismember(Item3, [Item3sorted;Item32sorted]);
-L(71:140) = L(71:140)+70
-NewError = NewError(L);
-
-% safe the new finaltest file with an additional column
-curdir = strcat('\\cnas.ru.nl\wrkgrp\STD-Back-Up-Exp2-EEG\', int2str(pNumber),'\Day 3\', int2str(pNumber),'_FinalTest');
-cd(curdir);
-outFileName=strcat(int2str(pNumber),'_FinalTest_new.txt');
-fid = fopen(outFileName,'w+');
-CC = {headline21,'NewError','\r\n'};
-headline21 = strjoin(CC, '\t');
-fprintf(fid,headline21);
-for i=1:140
-    fprintf(fid, '%d\t%d\t%d\t%s\t%s\t%s\t%d\t%d\t%d\t%d\t%d\t%d\t%s\t%d\t%d\r\n', Subj(i),Block(i),Trial(i),Item3{i} ,LbNL{i},label{i} ,Cond(i), VoiceOnset(i) , Marker(i) ,Error(i) ,PhonCorr(i),PhonIncorr(i) ,Produced{i},TypeError(i),NewError(i));
-end
-fclose(fid);
-
 % compute the new marker
-% this does not yet work!
+k = 1;
 for i=1:length(OldMarker)
-    % somehow jump the first I don't know how many lines that do not belong
-    % to the final test
-    for k=1:140
-    if Known{k}=='1' && Unknown{k}=='1'
-        tempMarker='1';
-    else
-        tempMarker='1';
+    if (strcmp(OldMarker{i},'S205') == 1) || strcmp(OldMarker{i},'S207') == 1 || strcmp(OldMarker{i},'S217') == 1 || (strcmp(OldMarker{i},'S206')==1 || strcmp(OldMarker{i},'S215') == 1) || (strcmp(OldMarker{i},'S216')==1)
+         if (ReadIn(k)==1 && k < 71)
+            OldMarker{i}='S205'; % usuable trial
+            k = k+1;
+         elseif (ReadIn(k)==1 && k > 70)
+            OldMarker{i}='S215'; % usuable trial
+            k = k+1;
+         elseif (ReadIn(k)==0 && k < 71)
+            OldMarker{i}='S206'; % not-usable trial
+            k = k+1;
+         elseif (ReadIn(k)==0 && k > 70)
+            OldMarker{i}='S216'; % not-usable trial
+            k = k+1;
+         end
     end
-    end
-    % somehow put the temp marker into the oldMarker to create the
-    % NewMarker vector 
-end
+end 
 
 % rewrite the new marker file
-outFileName=strcat(int2str(pNumber),'.vmrk');
+curdir = strcat('\\cnas.ru.nl\wrkgrp\STD-Back-Up-Exp2-EEG\', int2str(pNumber),'\Day3\EEG');
+cd(curdir);
+outFileName=strcat(int2str(pNumber),'_new.vmrk');
 fid = fopen(outFileName,'w+');
 fprintf(fid,headline1);
 fprintf(fid,headline2);
@@ -137,11 +74,16 @@ fprintf(fid,headline7);
 fprintf(fid,headline8);
 fprintf(fid,headline9);
 fprintf(fid,headline10);
-fprintf(fid,headline11);
+fprintf(fid,strrep(headline11, '\', '\\'));
+fprintf(fid,headline12);
+fprintf(fid,headline13);
 
-for i=1:length(Position)
-    temp=[Stimulus{i},', ', NewMarker{i},', ', num2str(Position(i)),', ',num2str(Length(i)),', ',Channel{i},'\r\n'];
+for i=1:length(OldMarker)
+    temp=[Stimulus{i},',', OldMarker{i},',', num2str(Position(i)),',',num2str(Length(i)),',',num2str(Channel(i)),'\r\n'];
     fprintf(fid,temp);
 end
 
 fclose(fid);
+
+% change this to your Github folder directory
+cd('U:\PhD\EXPERIMENT 2 - EEG\EEG-analysis');  
