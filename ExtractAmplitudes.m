9c96cd6d6f0c82968919bce9fdfddbab0ffbbcfb
amickan <a.mickan@donders.ru.nl>
2018-03-29 08:21:01 +0200

    save txt file

--- ExtractAmplitudes.m
+++ ExtractAmplitudes.m
@@ -2,11 +2,13 @@
 
 Average = zeros(27,2);
 for i = 1:27
-   Average[i,1] = subjects(i);
+   Average(i,1) = subjects(i);
    extract = mean(Condition1{i}.avg([13,40,42,43,44,39],[351:451]));
    average = mean(extract);
-   Averages[i,2] = mean(average);
+   Average(i,2) = mean(average);
    extract2 = mean(Condition2{i}.avg([13,40,42,43,44,39], [351:451]));
    average2 = mean(extract2);
-   Averages[i,3] = mean(average2);
+   Average(i,3) = mean(average2);
 end
+
+dlmwrite('ConditionAverages.txt',Average,'delimiter','\t');
